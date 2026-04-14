58b3c9cd380af83e11f8732e9a04a3322b164b0e
Nicklas Lykke Møller Jensen <Nicklaslmjensen@gmail.com>
2026-04-14 14:02:21 +0200

    Added gitignore and setup the database that i forgot with the initial commit D:

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,10 +1,17 @@
+using MediaDraftLeague.Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
