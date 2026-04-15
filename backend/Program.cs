fbfecd57d2adb0eb04b3d0c26ceca05a6407dd01
Nicklas Lykke Møller Jensen <Nicklaslmjensen@gmail.com>
2026-04-15 09:57:14 +0200

    updated Program.cs. Mainly a test commit

--- backend/Program.cs
+++ backend/Program.cs
@@ -8,6 +8,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -21,6 +22,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.MapControllers();
 }
 
 app.UseHttpsRedirection();
