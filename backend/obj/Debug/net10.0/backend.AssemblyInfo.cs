af8c2852a918e6975d2edbf1b15a9dec83cb70a2
Nicklas Lykke Møller Jensen <Nicklaslmjensen@gmail.com>
2026-05-01 09:50:41 +0200

    files that should have been ignored in the gitingore

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+832081300c71643e03048dd86d3ec4f8a565ec10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468a347f7a18ff35465a85d03c89ef1a3069248e")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
